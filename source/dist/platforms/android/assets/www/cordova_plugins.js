053303429ae4279029f942b0a894a19042e5753d
Travis Frisinger <travis.frisinger@chillisoft.co.za>
2014-12-19 10:11:11 +0200

    Added Platform and Plugins ( SQL and Local Notifications )

---
+++ source/dist/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/com.brodysoft.sqlitePlugin/www/SQLitePlugin.js",
+        "id": "com.brodysoft.sqlitePlugin.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "de.appplant.cordova.plugin.local-notification": "0.7.6",
+    "com.brodysoft.sqlitePlugin": "1.0.3",
+    "org.apache.cordova.device": "0.2.14-dev"
+}
+// BOTTOM OF METADATA
+});
